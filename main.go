34ebe46c43a9aeddd7b5ba5ea93c7163e44f4880
JohuZhu1982 <john@server31.io>
2024-01-31 19:38:59 +0800

    base gql

--- main.go
+++ main.go
@@ -8,8 +8,19 @@ import (
 
 func main() {
 	go gql.Process()
-	r := gin.Default()
-	err := r.Run(":9081")
+	gin.SetMode(gin.DebugMode)
+	router := gin.Default()
+	err := router.SetTrustedProxies([]string{"127.0.0.1"})
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	if err != nil {
+		return
+	}
+	err = router.Run(":9081")
 	if err != nil {
 		log.Println(err)
 	}
